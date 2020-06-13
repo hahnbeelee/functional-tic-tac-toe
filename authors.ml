2209e824237d80f4a7f13ba7b99da893ec102903
Hahnbee Lee <hl985@cornell.edu>
2020-06-12 21:08:15 -0400

    project set up

---
+++ authors.ml
@@ -0,0 +1,1 @@
+let hours_worked = [16; 18; 17]
---
+++ authors.mli
@@ -0,0 +1,7 @@
+(** Metadata about this submission.
+    @author Hahnbee Lee (hl985), Dhruv Sreenivas (ds844), Cyrus West (cjw297).
+*)
+
+(** [hours_worked] is a list of the number of hours each team member
+    worked on this assignment. *)
+val hours_worked : int list
